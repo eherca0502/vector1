2f78aa0194ea98c7973b60f4b6446d0a6bc07690
eherca0502 <ehc0502@outlook.com>
2023-08-24 21:53:40 -0600

    Agregar archivos de proyecto.

---
+++ vector1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vector1
+{
+     class SueldoOperarios
+    {
+        private int[] sueldos; // declaracion del vector
+
+        public void Cargar()
+        {
+            string linea;
+            sueldos = new int[5]; // creacion del vector
+            for(int f = 0; f < 5;f++)
+            {
+                Console.Write("Ingrese el sueldo:");
+                linea = Console.ReadLine();
+                sueldos[f] = int.Parse(linea);
+
+            }
+        }
+        public void Imprimir()
+        {
+            Console.WriteLine("Listado de todos los sueldos");
+            for (int f=0; f < 5;f++)
+            {
+                Console.WriteLine(sueldos[f]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            SueldoOperarios sueldooperarios = new SueldoOperarios();
+            sueldooperarios.Cargar();
+            sueldooperarios.Imprimir();
+            Console.ReadKey();
+        }
+    }
+}
